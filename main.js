537ecf4682b744498846b7453574e17dede0342b
ahanagithub <83067851+ahanagithub@users.noreply.github.com>
2021-07-27 12:37:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,61 +1,20 @@
-function ltf()
-{
-    //How to declare the arrays ? the id to be refer for innerHTML is declaration_of_arrays
-var fruits=["mango","apple","banana"];
-document.getElementById("declaration_of_arrays").innerHTML=fruits;
-    //How to fetch the first element of the array ? the id to be refer for innerHTML is access_first_element
-var first_fruit=fruits[0];
-document.getElementById("access_first_element").innerHTML=first_fruit;
-    //How to fetch the second element of the array ? the id to be refer for innerHTML is access_second_element
-    var second_fruit=fruits[1];
-    document.getElementById("access_second_element").innerHTML=second_fruit;
-    //How to fetch the third element of the array ? the id to be refer for innerHTML is access_third_element
-    var third_fruit=fruits[2];
-    document.getElementById("access_third_element").innerHTML=third_fruit;
-    //How to get the length of the array ? the id to be refer for innerHTML is display_length_of_array
-var length_arrey =fruits.length;
-document.getElementById("display_length_of_array").innerHTML=length_arrey;
-    //How to loop the array ? the id to be refer for innerHTML is loop_the_array
-    var array_value = "";
-     for (i = 0; i < fruits.length; i++)
-     
-     { 
-         console.log(fruits[i]); 
-          array_value = array_value + fruits[i] + "<br>";
-         } 
-         document.getElementById("loop_the_array").innerHTML = array_value;
-
-    //adding element at the end of the array using push method
-    //How to add element at the end of the array ? the id to be refer for innerHTML is add_element_at_the_end_of_array
-
-    //How to more elements at the end of the array ? the id to be refer for innerHTML is add_more_element_at_the_end_of_array
-
-    //How to declare array with alphabets ? the id to be refer for innerHTML is display_alphabets_array
-
-    //How to sort array with alphabets ? the id to be refer for innerHTML is display_sorted_alphabets_array
-
-    //How to reverse the array ? the id to be refer for innerHTML is display_reverse_array
-
-    //How to declare array with numbers ? the id to be refer for innerHTML is display_number_array
-
-    //How to find the maximum number from the array with numbers ? the id to be refer for innerHTML is display_maximum_number
-
-    //How to find the minimum number from the array with numbers ? the id to be refer for innerHTML is display_minimum_number
-
-    console.log("ADDITIONAL ACTIVITY");
-
-    //Use unshift method to add element at the start of the array . the id to be refer for innerHTML is add_element_at_the_start_of_array
-
-    //Use unshift method to add more elements at the start of the array . the id to be refer for innerHTML is add_more_element_at_the_start_of_array
-
-    //Use shift method to remove elements from the start of the array . the id to be refer for innerHTML is remove_element_from_start_of_array
-
-    //Use pop method to remove elements from the end of the array . the id to be refer for innerHTML is remove_element_from_end_of_array
-
-    //How declare array with first world country ? 
-
-    //How declare array with second world country ? 
-
-    //use concat method to merge two arrays . the id to be refer for innerHTML is merge_two_arrays
-
+var images = [
+    "https://image.shutterstock.com/image-vector/cute-little-girl-spring-260nw-337904864.jpg"
+    ,
+    "https://thumbs.dreamstime.com/b/hipster-mom-baby-pretty-young-style-mother-glasses-holding-hugging-her-cute-little-daughter-laughing-38641979.jpg"
+    ,
+    "https://c8.alamy.com/comp/W3KX0F/family-single-father-with-little-daughter-cartoon-W3KX0F.jpg"
+    ,
+    "https://c8.alamy.com/comp/H5C8YD/parents-and-daughter-family-design-H5C8YD.jpg"
+  ];
+  
+  var i = 0;
+  function nextslide()
+  { 
+    if(i>3)
+    { 
+      i=0; 
+    }
+    document.getElementById("album").src = images[i];
+    i++;
 }
